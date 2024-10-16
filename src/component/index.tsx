bbd686aac3cc16f20b12914a1776a107bd3b768b
faroukq1 <faroukq1@gmail.com>
2024-10-16 13:54:39 +0100

    first commit

--- src/component/index.tsx
+++
@@ -1,7 +0,0 @@
-export { default as Navbar } from "./Navbar";
-export { default as Hero } from "./Hero";
-export { default as Projets } from "./Projets";
-export { default as Contact } from "./Contact";
-export { default as CustomInput } from "./CustomInput";
-export { default as TooggleDarkMode } from "./ToggleDarkMode";
-export { default as ProjectPictureModal } from "./ProjectPictureModal";
